04c9decb171aeb9cbe2ca58de10b2b7a43725a3c
ZhannaZucher <jzucher@gmail.com>
2024-02-25 11:20:45 +0100

    feat : make face-snap component reusable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,32 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  mySecondSnap!: FaceSnap;
+  myThirdSnap!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      "Mon meilleur ami d'enfance",
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    );
+    this.mySecondSnap = new FaceSnap(
+      'Serge',
+      'Mon frère adoré',
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    );
+    this.myThirdSnap = new FaceSnap(
+      'Nounours',
+      'Meilleur doudou pour dormir',
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    );
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -8,34 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss',
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
+  //on crée une propriété personnalisée qui peut être utilisée depuis extérieur grâce au décorateur @Input
+  // synthaxe:
+  //@Input nom de la propriété : son type
+  @Input() faceSnap!: FaceSnap;
+  //on déclare les props statiques
   isSnapped!: boolean;
   buttonText!: string;
-  imageUrl!: string;
 
   //La méthode  ngOnInit()  est appelée automatiquement par Angular au moment de la création de chaque instance du component. Elle permet notamment d'initialiser des propriétés.
   ngOnInit() {
-    this.title = 'Archibald';
-    this.description = "Mon meilleur ami d'enfance";
-    this.createdDate = new Date();
-    this.snaps = 6;
+    //ici on initialise que les propriétés statiques du composant
     this.isSnapped = false;
     this.buttonText = 'Oh snap!';
-    this.imageUrl =
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
   }
 
   //la méthode qui permet d'ajouter un snap à l'image on Click
   onSnap() {
     if (!this.isSnapped) {
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.isSnapped = true;
       this.buttonText = 'Oops, unSnap!';
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.buttonText = 'Oh snap!';
       this.isSnapped = false;
     }
