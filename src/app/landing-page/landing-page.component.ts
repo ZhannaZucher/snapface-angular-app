9e1fd7c7a80475b4cd8d151e032f7f3d564310c5
ZhannaZucher <jzucher@gmail.com>
2024-03-12 17:35:27 +0100

    add: form with one input

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  userEmail!: string;
+  //on peut déclarer ici la valeur par défaut :
+  // userEmail: string = 'fofo@gmail.com';
+
   constructor(private router: Router) {}
 
   onClick(): void {
     this.router.navigateByUrl('facesnaps');
   }
+
+  onSubmitForm(): void {
+    console.log(this.userEmail);
+  }
+  ngOnInit(): void {}
 }
