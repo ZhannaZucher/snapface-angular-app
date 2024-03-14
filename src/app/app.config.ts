cf9ca5521e005d7d096ff771f2d349bfdde82a4f
ZhannaZucher <jzucher@gmail.com>
2024-03-14 12:04:56 +0100

    add: HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { LOCALE_ID } from '@angular/core';
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), { provide: LOCALE_ID, useValue: 'fr-FR' }],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
+  ],
 };
