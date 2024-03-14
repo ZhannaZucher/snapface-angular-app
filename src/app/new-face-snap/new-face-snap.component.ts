b1b7a4e943d198c84d7b8079763030c60e6d2cb2
ZhannaZucher <jzucher@gmail.com>
2024-03-14 09:13:06 +0100

    add: new snap preview in form

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { FaceSnap } from '../models/face-snap.model';
+import { AsyncPipe, DatePipe, NgIf, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-new-face-snap',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf, AsyncPipe, UpperCasePipe, DatePipe],
   templateUrl: './new-face-snap.component.html',
   styleUrl: './new-face-snap.component.scss',
 })
 export class NewFaceSnapComponent implements OnInit {
   snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
 
   constructor(private formBuilder: FormBuilder) {}
 
@@ -20,6 +24,16 @@ export class NewFaceSnapComponent implements OnInit {
       imageUrl: [null],
       location: [null],
     });
+    //on initialise l'observable sur le formulaire onChange sur ses valeurs
+    //comme dans le formulaire il manque id, createdDate et snaps, alors qu'on observe objet type FaceSnap surlequel ces propriétés existent, on modifie l'émission des valeurs avec map pour ajouter les proriétés manquantes
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map((formValue) => ({
+        ...formValue,
+        createdDate: new Date(),
+        id: 0,
+        snaps: 0,
+      }))
+    );
   }
 
   onSubmitForm(): void {
